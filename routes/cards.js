e5214d6b5160b2dbcef874518223d4f5ab709a1a
Elya Tumanova <elatumanova@yandex.ru>
2023-09-09 16:04:41 +0300

    sprint 14 5

--- routes/cards.js
+++ routes/cards.js
@@ -13,7 +13,7 @@ createCardRrouter.post('/cards',
 celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().regex(/^(http|https):\/\/(\w|[-\._~:\/\?#\[\]@!\$&'\(\)\*\+,;=])|(#$)/),
   }),
 }),
 createCard); 
