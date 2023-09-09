5d3aa7d89ff7a1cf1df643021af40aa207f53568
Elya Tumanova <elatumanova@yandex.ru>
2023-09-09 14:06:16 +0300

    2

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,8 @@ const likeCardRouter = require('express').Router();
 const dislikeCardRouter = require('express').Router();
 const { getCards, createCard,deleteCardById,likeCard,dislikeCard } = require('../controllers/cards');
 
+const { celebrate, Joi } = require('celebrate');
+
 getCardsRouter.get('/cards', getCards);
 
 createCardRrouter.post('/cards', 
